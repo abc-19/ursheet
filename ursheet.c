d96930bb00a58042031d2ebcb27bd3df9752d921
abc19 <juan.patino19m@gmail.com>
2024-12-27 09:22:33 -0500

    checking for a limited number of tokens

--- ursheet.c
+++ ursheet.c
@@ -8,22 +8,89 @@
  *	     `---`
  */
 #include <err.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <getopt.h>
 #include <assert.h>
 
-// TODO: replace all asserts checkers
+#define	u8	unsigned char
+#define	u16	unsigned short
+#define	u32	unsigned int
+#define	u64	unsigned long
+
+#define	i8	signed char
+#define	i16	signed short
+#define	i32	signed int
+#define	i64	signed long
+
+#define	FAMILY_SIZE	64
+
+enum TokenKind
+{
+	TokenIsString		= '"',
+	TokenIsReference	= '@',
+	TokenIsDelimiter	= '|',
+	TokenIsAdd			= '+',
+	TokenIsMul			= '*',
+	TokenIsDiv			= '/',
+	TokenIsSub			= '-',
+	TokenIsExpr			= '=',
+	TokenIsClone		= '^',
+	TokenIsNumber,
+};
+
+enum CellKind
+{
+	CellIsEmpty		= 0,
+	CellIsError		= 1,
+	CellIsNumber	= 2,
+	CellIsText		= 3
+};
+
+enum CellErrs
+{
+	ErrCellOverflow	= 0,
+};
+
+struct Cell;
+
+union As
+{
+	const long double	num;
+	const char			*txt;
+	const struct Cell	*ref;
+};
+
+struct Token
+{
+	union	As as;
+	enum	TokenKind kind;
+};
+
+struct Cell
+{
+	struct	Token family[FAMILY_SIZE];
+	union	As as;
+	u16		nthT;
+	enum	CellKind kind;
+};
 
 struct UrSh
 {
-	struct	{ unsigned short rows, cols; } sz;
+	struct	{ u16 rows, cols; } sz;
+	struct	Cell *grid;
 	size_t	length;
 	char	*filename, *src;
 };
 
 static void readContents (struct UrSh *const, FILE*);
-static void getTableDimensions (const char*, unsigned short*, unsigned short*);
+static void getTableDimensions (const char*, u16*, u16*);
+
+static void lexTable (struct UrSh *const);
+static void pushTokenIntoCell (struct Cell *const, enum TokenKind);
+
+static void setCell2Error (struct Cell *const, u8);
 
 int main (int argc, char **argv)
 {
@@ -31,13 +98,17 @@ int main (int argc, char **argv)
 		errx(EXIT_SUCCESS, "usage: %s <sheet>\nThat simple ;)", *argv);
 
 	struct UrSh us = {0};
-
 	us.filename = argv[1];
 
 	readContents(&us, fopen(us.filename, "r"));
 	getTableDimensions(us.src, &us.sz.rows, &us.sz.cols);
 
-	printf("%d %d", us.sz.rows, us.sz.cols);
+	us.grid = (struct Cell*) calloc(us.sz.rows * us.sz.cols, sizeof(struct Cell));
+	assert(us.grid && "cannot allocate memory");
+
+	lexTable(&us);
+	free(us.grid);
+	free(us.src);
 
 	return 0;
 }
@@ -52,7 +123,7 @@ static void readContents (struct UrSh *const us, FILE *file)
 	fseek(file, 0, SEEK_SET);
 
 	us->src = (char*) calloc(1 + us->length, 1);
-	assert(us->src);
+	assert(us->src && "cannot allocate memory");
 
 	const size_t BRead = fread(us->src, 1, us->length, file);
 
@@ -62,9 +133,9 @@ static void readContents (struct UrSh *const us, FILE *file)
 	fclose(file);
 }
 
-static void getTableDimensions (const char *src, unsigned short *row, unsigned short *col)
+static void getTableDimensions (const char *src, u16 *row, u16 *col)
 {
-	unsigned short mcol = 0;
+	u16 mcol = 0;
 
 	while (*src) {
 		switch (*src++) {
@@ -80,4 +151,49 @@ static void getTableDimensions (const char *src, unsigned short *row, unsigned s
 	}
 
 	*col = (*col > mcol) ? *col : mcol;
+}
+
+static void lexTable (struct UrSh *const us)
+{
+	struct Cell *currentCell = &us->grid[0];
+
+	for (size_t k = 0; k < us->length; k++) {
+		const char chr = us->src[k];
+
+		switch (chr) {
+			case '+':
+			case '/':
+			case '*':
+			case '^':
+			case '=': pushTokenIntoCell(currentCell, chr); break;
+
+			case 10 : break;
+			case '|': break;
+			case '"': break;
+			case '@': break;
+		}
+	}
+
+	printf("%s\n", currentCell->as.txt);
+}
+
+static void pushTokenIntoCell (struct Cell *const cc, enum TokenKind kind)
+{
+	if (cc->nthT == 0) {
+		setCell2Error(cc, ErrCellOverflow);
+		return;
+	}
+	
+	struct Token* this = &cc->family[cc->nthT++];
+	this->kind = kind;
+}
+
+static void setCell2Error (struct Cell *const cc, u8 which)
+{
+	static const char *const errors[] = {
+		"!overflow"
+	};
+
+	cc->as.txt = errors[which];
+	cc->kind = CellIsError;
 }
