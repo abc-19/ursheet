7b735bf87b244f7bd2b705082d691b19f82be2a6
abc19 <juan.patino19m@gmail.com>
2024-12-27 08:11:37 -0500

    abc

---
+++ ursheet.c
@@ -0,0 +1,16 @@
+/*
+ *	 __         __
+ *	/  \.-"""-./  \		UrSheet - main file.
+ *	\    -   -    /		abc19
+ *	 |   o   o   |		Dec 27 2024
+ *	 \  .-'''-.  /
+ *	  '-\__Y__/-'
+ *	     `---`
+ */
+#include <stdio.h>
+
+int main ()
+{
+	printf("Hello World, UrSheet\n");
+	return 0;
+}
